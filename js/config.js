6782866952585b0a2109e99c76cc951a5b67510e
Madhu Gowda <66135566+mb-maddy@users.noreply.github.com>
2025-03-16 13:03:14 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_1kDOWeR6C', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '55cca2mjm0do0s6uh9kq1pes8q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
